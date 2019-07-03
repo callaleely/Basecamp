49e639d43c2fe5a8a6fbd629b5cf3971e198fb9d
Calla <jiyoung8751@gmail.com>
2019-07-03 10:16:15 -0700

    Backend auth finish - routes, controllers, model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def create
+        @user = User.new(user_params)
+    end
+
+    private 
+    
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,9 +27,13 @@ class User < ApplicationRecord
   end
 
   def reset_session_token
-    session[:session_token]
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
   end
 
   def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
   end
+  
 end
