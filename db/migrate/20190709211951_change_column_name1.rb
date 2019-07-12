c3f58f5150fd5135a2b0f989544261a438244848
Calla <jiyoung8751@gmail.com>
2019-07-12 09:25:13 -0700

    Remove change column migration

--- db/migrate/20190709211951_change_column_name1.rb
+++ db/migrate/20190709211951_change_column_name1.rb
@@ -1,6 +1,6 @@
 class ChangeColumnName1 < ActiveRecord::Migration[5.2]
   def change
     rename_column :scopes, :type, :category
-    rename_column :events, :repear, :repeat
+    # rename_column :events, :repear, :repeat
   end
 end
