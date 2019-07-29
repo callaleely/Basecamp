533c3013bf816b6194788631749dd7387f7c2b31
Calla <jiyoung8751@gmail.com>
2019-07-29 15:31:59 -0700

    Change scope form components

--- frontend/components/scope/scope_form.jsx
+++ frontend/components/scope/scope_form.jsx
@@ -4,13 +4,30 @@ import React from 'react';
 class ScopeForm extends React.Component {
     constructor(props) {
         super(props);
+    }
 
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.createScope()
+    }
+
+    update(property) {
+        return e => this.setState({[property]: e.currentTarget.value})
     }
 
     render() {
         return (
             <div>
-
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Title:
+                        <br/>
+                        <input type="text"
+                            value="title"
+                            onChange={this.forceUpdate("title")}/>
+                    </label>
+                </form>
             </div>
         )
     }
