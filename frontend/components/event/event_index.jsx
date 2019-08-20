6030900166d0328a440025c20ecfdad9509d33d1
Calla <jiyoung8751@gmail.com>
2019-08-20 11:15:17 -0700

    Fix the calendar by importing the calendar style

--- frontend/components/event/event_index.jsx
+++ frontend/components/event/event_index.jsx
@@ -19,20 +19,29 @@ const localizer = momentLocalizer(moment) // or globalizeLocalizer
 
 class EventIndex extends React.Component {
 
+    componentDidMount() {
+        this.props.fetchEvents(this.props.match.params.scopeId);
+        this.props.fetchScopes();
+    }
+
     setDate(date) {
         this.setState({date})
     };
 
     render() {
         let scopePage = '/scopes/'+this.props.match.params.scopeId
-        
+        let scopeName = this.props.scopes.map(scope => {
+            if (scope.id == this.props.id) {
+                return scope.name
+            }
+        });
         return(
             <div className="feature_page">
                 <NavBarContainer />
                     <div className="feature_container">
                         <div className="scope_name_container">
                             <Link to={scopePage} className="scope_name">
-                                {this.props.scopeName}
+                                {scopeName}
                             </Link>
                         </div>
                         <div className="feature_index"> 
--- frontend/components/event/event_index_container.jsx
+++ frontend/components/event/event_index_container.jsx
@@ -3,16 +3,21 @@ import EventIndex from './event_index';
 import {
     fetchEvents
 } from '../../actions/event_actions';
+import {
+    fetchScopes
+} from '../../actions/scope_actions';
 
 const mapStateToProps = (state, ownProps) => { 
     return {
     events: Object.keys(state.entities.events)
         .map(id => state.entities.events[id]),
-    scopeName: state.entities.scopes[ownProps.match.params.scopeId].name
+    scopes: Object.values(state.entities.scopes),
+    id: ownProps.match.params.scopeId,
 }};
 
 const mapDispatchToProps = dispatch => ({
-    fetchEvents: (scopeId) => dispatch(fetchEvents(scopeId))
+    fetchEvents: (scopeId) => dispatch(fetchEvents(scopeId)),
+    fetchScopes: () => dispatch(fetchScopes())
 })
 
 export default connect(
