4516a2cd6bfe33d9bf4f81c4bd8a821e8b5e7ad8
Calla <jiyoung8751@gmail.com>
2019-08-20 10:37:45 -0700

    Add delete button styling to the each card

--- frontend/components/scope/scope_index_item.jsx
+++ frontend/components/scope/scope_index_item.jsx
@@ -4,17 +4,27 @@ import {withRouter, Link} from 'react-router-dom';
 class ScopeIndexItem extends React.Component {
     constructor(props) {
         super(props);
+        this.deleteScope = this.deleteScope.bind(this);
         // this.handleClick = this.handleClick.bind(this);
+    }
+
+    deleteScope() {
+        this.props.deleteScope(this.props.scope.id)
     }
 
     render () {
         return (
             <div className="card-content">
+                <div className="card-content-flex">
                 <Link className="card-title" to={`/scopes/${this.props.scope.id}`}>
                     {this.props.scope.name}
+                </Link>
+                <button onClick={this.deleteScope}>
+                <img src={window.images.delete} height="12" />
+                </button>
+                </div>
                     <br/>
                     <p>{this.props.scope.description}</p>
-                </Link>
 
             </div>
         )
